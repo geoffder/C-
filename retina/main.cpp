0dabcaa6cce0d9521f4fb5ab383746d7c1ce83e5
Geoff deRosenroll <geoffder@uvic.ca>
2019-03-16 23:53:30 -0700

    trying to build the cell class as practice

--- retina/main.cpp
+++ retina/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <Eigen/Dense>
+#include <tuple>
+#include <vector>
+
+using Eigen;
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+class Cell {
+public:
+    NetworkModel model;  // the network model this cell belongs to
+    std::tuple<int, int> pos;  // centre coordinates (constant)
+    int diam;  // soma diameter
+    Eigen::MatrixXi somaMask;
+    int rf  // receptive field radius
+    Eigen::MatrixXi rfMask  //
+    float Vm = 0
+    float dtau  // decay tau
+    std::vector<float> rec
+    std::vector<std::vector<float>> recs
+};
